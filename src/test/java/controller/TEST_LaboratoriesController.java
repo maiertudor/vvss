79f649ccb4f2bb1e30f93760bc37023fbf244d3d
TudorM <maiert69@gmail.com>
2018-04-01 19:17:35 +0300

    New tests

--- src/test/java/controller/TEST_LaboratoriesController.java
+++ src/test/java/controller/TEST_LaboratoriesController.java
@@ -3,20 +3,25 @@ package controller;
 import model.Laboratory;
 import model.Student;
 import org.junit.Test;
+import repository.FileDataPersistence;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TEST_LaboratoriesController {
 
     private final LaboratoriesController controller;
+    private final FileDataPersistence laboratoryPersistence;
 
     public TEST_LaboratoriesController() {
         controller = new LaboratoriesController("students.txt", "laboratories.txt");
+        laboratoryPersistence = new FileDataPersistence("laboratories.txt");
     }
 
     @Test
@@ -41,7 +46,42 @@ public class TEST_LaboratoriesController {
             Laboratory laboratory = new Laboratory(2, dateString, 2, "asdf1234");
             assertTrue(controller.saveLaboratory(laboratory));
         } catch (ParseException e) {
-            assertTrue(false);
+            fail();
         }
+    }
+
+    @Test
+    public void testAddGrade() {
+        try {
+            assertTrue(controller.addGrade("asdf1234", "2", 9));
+        } catch (IOException | ParseException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetPassingStudents() {
+        try {
+            List<Student> passedStudents = controller.passedStudents();
+            assertNotNull(passedStudents);
+            assertFalse(passedStudents.isEmpty());
+            for (Student passingStudent : passedStudents) {
+                float midGrade = getStudentMidGrade(passingStudent);
+                assertTrue(midGrade >= 5);
+                System.out.println(passingStudent.getName() + " passed with " + midGrade);
+            }
+
+        } catch (IOException | ParseException e) {
+            fail();
+        }
+    }
+
+    private float getStudentMidGrade(Student passingStudent) throws IOException, ParseException {
+        List<Laboratory> studentLabs = laboratoryPersistence.getLaboratoryMap().get(passingStudent.getRegNumber());
+        float midGrade = studentLabs.get(0).getGrade();
+        for (Laboratory laboratory : studentLabs) {
+            midGrade = (midGrade + laboratory.getGrade()) / 2;
+        }
+        return midGrade;
     }
 }
